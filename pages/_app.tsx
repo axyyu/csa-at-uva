7da4e36f8868b549e52361eacf5be15bff86b077
Andrew Wang <andrew@pop-os.localdomain>
2020-07-17 18:09:19 -0400

    Setup Typescript and Sass

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from "next/app";
+
+import "../styles/global.scss";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
