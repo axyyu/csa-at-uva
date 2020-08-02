a3558127e4a9fdcc7636cd91cc3937d181c178f8
Andrew Wang <andrewxiaoyu0@gmail.com>
2020-08-01 21:36:12 -0400

    Remove events server props

--- pages/events.jsx
+++ pages/events.jsx
@@ -18,21 +18,8 @@ const Events = (props) => {
 };
 
 export async function getServerSideProps(context) {
-  const proto = context.req.connection.encrypted ? "https" : "http";
-  const baseUrl = `${proto}://${context.req.headers.host}/api/events`;
-  const res = await fetch(baseUrl);
-  let data = null;
-  try {
-    data = await res.json();
-    data = data.items;
-    if (data == null) {
-      data = [];
-    }
-  } catch (err) {
-    data = [];
-  }
   return {
-    props: { events: data }, // will be passed to the page component as props
+    props: { events: [] }, // will be passed to the page component as props
   };
 }
 
