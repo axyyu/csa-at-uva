ad08386e342acacf263c389276fd016cad309fad
Andrew Wang <andrewxiaoyu0@gmail.com>
2020-08-19 11:59:23 -0400

    Update mobile navbar

--- layouts/Navbar.jsx
+++ layouts/Navbar.jsx
@@ -1,58 +1,77 @@
 import Link from 'next/link';
-import { FaFacebook, FaInstagram, FaYoutube, FaBars } from 'react-icons/fa';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaBars,
+  FaTimes,
+} from 'react-icons/fa';
 
-const Navbar = () => {
-  return (
-    <aside className='navbar'>
-      <img className='logo' src='/Official-CSA-Square-Logo.png'></img>
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <div className='bars'>
-        <FaBars />
-      </div>
+    this.state = {
+      show: false,
+    };
+  }
+  toggleNav = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    const display = this.state.show ? 'block' : 'none';
+    return (
+      <aside className='navbar'>
+        <img className='logo' src='/Official-CSA-Square-Logo.png'></img>
 
-      <nav>
-        <ul className='nav-list'>
-          <li>
-            <Link href='/'>
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/events'>
-              <a>Events</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/officers'>
-              <a>Officers</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/families'>
-              <a>Families</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/alumni'>
-              <a>Past Years</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/gallery'>
-              <a>Gallery</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className='social-container'>
-        <div className='social-links'>
-          <FaFacebook />
-          <FaInstagram />
-          <FaYoutube />
+        <div className='bars' onClick={this.toggleNav}>
+          {this.state.show ? <FaTimes /> : <FaBars />}
         </div>
-      </div>
-    </aside>
-  );
-};
+
+        <nav className={display}>
+          <ul className='nav-list'>
+            <li>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/events'>
+                <a>Events</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/officers'>
+                <a>Officers</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/families'>
+                <a>Families</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/alumni'>
+                <a>Past Years</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/gallery'>
+                <a>Gallery</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={`social-container ${display}`}>
+          <div className='social-links'>
+            <FaFacebook />
+            <FaInstagram />
+            <FaYoutube />
+          </div>
+        </div>
+      </aside>
+    );
+  }
+}
 
 export default Navbar;
